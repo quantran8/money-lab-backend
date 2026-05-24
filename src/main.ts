96d0872ff17d9e8f792fa395df58ec022f9272c2
Alan <quan.tran@bee-tech.ai.gitconfig>
2026-05-24 14:35:18 +0700

    update port value

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,15 @@ import {
 } from '@nestjs/platform-fastify';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-// import { CamelCaseInterceptor } from './common/interceptors/camel-case.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
   app.setGlobalPrefix('api/v1');
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -20,7 +21,12 @@ async function bootstrap() {
       transformOptions: { enableImplicitConversion: true },
     }),
   );
-  // app.useGlobalInterceptors(new CamelCaseInterceptor());
-  await app.listen(5000);
+
+  const port = 5000;
+
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`Server running on port ${port}`);
 }
+
 bootstrap();
